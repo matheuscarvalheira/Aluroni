37af9e9bdff39881a92acf0613492ed5c5555632
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-08-22 17:42:31 -0300

    implementing the default page

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cardapio from './pages/Cardapio';
 import Inicio from './pages/Inicio';
 import Menu from 'components/Menu';
+import PaginaPadrao from './components/PaginaPadrao';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Menu />
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/cardapio" element={<Cardapio />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path="cardapio" element={<Cardapio />} />
+          </Route>
+        </Routes>
+      </Router>
+    </main>
   );
 }
