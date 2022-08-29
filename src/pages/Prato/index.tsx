21d2348804dfdc5ab9076366a0acf5cebb713435
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-08-29 14:58:14 -0300

    dish rendering along with header

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -3,33 +3,30 @@ import { useParams, useNavigate } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
 import TagsPrato from 'components/TagsPrato';
 import NotFound from 'pages/NotFound';
+import PaginaPadrao from 'components/PaginaPadrao';
 
-export default function Prato(){
+export default function Prato() {
   const { id } = useParams();
   const navigate = useNavigate();
-  const prato = cardapio.find(item => item.id === Number(id));
-  if(!prato){
-    return <NotFound/>;
+  const prato = cardapio.find((item) => item.id === Number(id));
+  if (!prato) {
+    return <NotFound />;
   }
   return (
-    <>
-      <button className={styles.voltar} onClick= {() => navigate(-1)}>
+    <PaginaPadrao>
+      <button className={styles.voltar} onClick={() => navigate(-1)}>
         {'< Voltar'}
       </button>
-      <section className={ styles.container }>
-        <h1 className={ styles.titulo}>
-          {prato.title}
-        </h1>
-        <div className={ styles.imagem}>
+      <section className={styles.container}>
+        <h1 className={styles.titulo}>{prato.title}</h1>
+        <div className={styles.imagem}>
           <img src={prato.photo} alt={prato.title} />
         </div>
-        <div className={ styles.conteudo}>
-          <p className={ styles.conteudo__descricao}>
-            {prato.description}
-          </p>
-          <TagsPrato {...prato}/>
+        <div className={styles.conteudo}>
+          <p className={styles.conteudo__descricao}>{prato.description}</p>
+          <TagsPrato {...prato} />
         </div>
       </section>
-    </>
+    </PaginaPadrao>
   );
 }
