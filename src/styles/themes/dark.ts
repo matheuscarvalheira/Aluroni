bc73e3d0f12e76a32590bf68e3230e3ba50a0224
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-09-08 18:42:59 -0300

    setting light and dark theme

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#fff',
+    secundary: '#54defd',
+
+    background: '#04080f',
+    text: '#ffff'
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#e8f7ee',
+    secundary: '#e8f7ee',
+
+    background: '#f5f5f5',
+    text: '#000'
+  },
+};
