6ec0bb772f71bb1cdbc7503a221c09c382124415
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-09-08 18:41:04 -0300

    using state and ThemeProvider

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,23 +7,38 @@ import Sobre from 'pages/Sobre';
 import Footer from 'components/Footer';
 import NotFound from 'pages/NotFound';
 import Prato from 'pages/Prato';
+import { ThemeProvider } from 'styled-components';
+import { useState } from 'react';
+import light from 'styles/themes/light';
+import dark from 'styles/themes/dark';
+import GlobalStyles from './styles/global';
+
 
 export default function AppRouter() {
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light );
+  };
+  
   return (
-    <main className="container">
-      <Router>
-        <Menu />
-        <Routes>
-          <Route path="/" element={<PaginaPadrao />}>
-            <Route index element={<Inicio />} />
-            <Route path="cardapio" element={<Cardapio />} />
-            <Route path="sobre" element={<Sobre />} />
-          </Route>
-          <Route path="prato/:id" element={<Prato />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </main>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <main className="container">
+        <Router>
+          <Menu toggleTheme={toggleTheme}/>
+          <Routes>
+            <Route path="/" element={<PaginaPadrao />}>
+              <Route index element={<Inicio />} />
+              <Route path="cardapio" element={<Cardapio />} />
+              <Route path="sobre" element={<Sobre />} />
+            </Route>
+            <Route path="prato/:id" element={<Prato />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </main>
+    </ThemeProvider>
   );
 }
