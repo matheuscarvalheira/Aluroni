ec52c082f21c6da3ed74ba9f0cb3b6ae9b9d62fd
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2023-10-06 13:00:03 -0300

    adjusting search engine performance

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { memo, useMemo } from 'react';
 import styles from './Buscador.module.scss';
-import {CgSearch} from 'react-icons/cg';
+import { CgSearch } from 'react-icons/cg';
 
 interface Props {
   busca: string;
   setBusca: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function Buscador({busca, setBusca}: Props) {
+function Buscador({ busca, setBusca }: Props) {
+  const elemento = useMemo(() => {
+    return <CgSearch size={20} color="#4C4D5E" />;
+  }, []);
   return (
     <div className={styles.buscador}>
-      <input 
+      <input
         value={busca}
         onChange={(evento) => setBusca(evento.target.value)}
         placeholder="Buscar 🍥"
-    
       />
-      <CgSearch
-        size={20}
-        color="#4C4D5E"
-      />
+      {elemento}
     </div>
-  
   );
 }
+
+export default memo(Buscador);
